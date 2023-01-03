23b97e59806604fc298ae4d3be197028686f1cbe
Bailey McDonald <mcdonald.bailey@gmail.com>
2023-01-03 12:48:44 -0600

    django mixtape api built out with playlist model

--- django_mixtape/playlists_api/models.py
+++ django_mixtape/playlists_api/models.py
@@ -7,6 +7,14 @@ class Playlist(models.Model):
     author = models.CharField(max_length=255)
     summary = models.TextField()
     
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artist = models.CharField(max_length=255)
+    cover = models.CharField(max_length=255)
+    album = models.CharField(max_length=255)
+    playlist = models.ForeignKey(Playlist, on_delete=models.CASCADE)
+
+
 
 
 # title
